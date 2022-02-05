22649b32b237808adf5357e21a0b26bbaf856734
timmson <timmson666@mail.ru>
2022-02-05 22:15:29 +0300

    Initial commit

--- src/app.js
+++ src/app.js
@@ -1,20 +1,21 @@
 import React from "react"
 
-const months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Ноябрь", "Декабрь",]
+const months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Ноябрь", "Декабрь"]
+
 
 const zodiacsAndMonths = [
-	"Овен (с 21 марта по 20 апреля)",
-	"Телец (с 21 по 21 мая)",
-	"Близнецы (с 22 мая по 21 июня)",
-	"Рак (с 22 июня по 22 июля)",
-	"Лев (с 23 июля по 21 августа)",
-	"Дева (с 22 августа по 23 сентября)",
-	"Весы (с 24 сентября по 23 октября)",
-	"Скорпион (с 24 октября по 22 ноября)",
-	"Стрелец (с 23 ноября по 22 декабря)",
-	"Козерог (с 23 декабря по 20 января)",
-	"Водолей (с 21 января по 19 февраля)",
-	"Рыбы (с 20 февраля по 20 марта)"
+	{title: "♈", dates: "21 марта - 20 апреля"},
+	{title: "♉", dates: "21 апреля - 21 мая"},
+	{title: "♊", dates: "22 мая - 21 июня"},
+	{title: "♋", dates: "22 июня - 22 июля"},
+	{title: "♌", dates: "23 июля - 21 августа"},
+	{title: "♍", dates: "22 августа - 23 сентября"},
+	{title: "♎", dates: "24 сентября - 23 октября"},
+	{title: "♏", dates: "24 октября - 22 ноября"},
+	{title: "♐", dates: "23 ноября - 22 декабря"},
+	{title: "♑", dates: "23 декабря - 20 января"},
+	{title: "♒", dates: "21 января - 19 февраля"},
+	{title: "♓", dates: "20 февраля - 20 марта"}
 ]
 
 function randomIntFromInterval(length) {
@@ -37,43 +38,27 @@ export default function App() {
 
 	const zodiacs = zodiacsAndMonths.map((zodiac) => {
 		const [success, warning, danger] = generate(date)
-		return (<tr key={zodiac}>
-			<td>{zodiac}</td>
-			<td className="table-success">{success.join(", ")}</td>
-			<td className="table-warning">{warning.join(", ")}</td>
-			<td className="table-danger">{danger.join(", ")}</td>
-		</tr>)
+		return (<div key={zodiac.title} className="row mx-1 text-center">
+			<div className="col-3 border border-dark">
+				<p className={"fw-bold fs-1"}>{zodiac.title}</p>
+				<p>({zodiac.dates})</p>
+			</div>
+			<div className="col-3 bg-success text-light border border-dark">{success.join(", ")}</div>
+			<div className="col-3 bg-warning text-dark border border-dark">{warning.join(", ")}</div>
+			<div className="col-3 bg-danger text-light border border-dark">{danger.join(", ")}</div>
+		</div>)
 	})
-
 
 	return (
 		<div id="app" className="container">
 			<h2 className="text-center">Когда лучше деплоить на продакшн, {months[date.getMonth()]} {date.getFullYear()} г.</h2>
-			<div className="row">
-				<div className="col">
-					<table className="table text-center border border-dark">
-						<thead>
-							<tr>
-								<td>
-								Знак Зодиака
-								</td>
-								<td className="table-success">
-								Благоприятные дни
-								</td>
-								<td className="table-warning">
-								Нейтральные дни
-								</td>
-								<td className="table-danger">
-								Неблагоприятные дни
-								</td>
-							</tr>
-						</thead>
-						<tbody>
-							{zodiacs}
-						</tbody>
-					</table>
-				</div>
+			<div className="row mx-1 text-center">
+				<div className="col-3 border border-dark">Знак Зодиака</div>
+				<div className="col-3 bg-success text-light border border-dark">Супер</div>
+				<div className="col-3 bg-warning text-dark border border-dark">Можно</div>
+				<div className="col-3 bg-danger text-light border border-dark">Не стоит</div>
 			</div>
+			{zodiacs}
 		</div>
 	)
 }
